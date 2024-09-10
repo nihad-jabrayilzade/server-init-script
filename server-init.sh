b77d8a7ce66b0048673de0fda7071b533b34c3ad
Nihad Jabrayilzade <nihad@Nihads-MacBook-Pro.local>
2024-09-11 00:49:13 +0200

    refactoring

--- server-init.sh
+++ server-init.sh
@@ -1,102 +1,123 @@
 #!/bin/bash
 
-log_message() {
+read -p "Enter the username of the new user: " USER
+read -p "Enter the email address for the new user's SSH key: " USER_EMAIL
+
+USER_DIR="/home/$USER"
+PROJECT_NAME="authomatify"
+
+log() {
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
 }
 
-# Check if the script is running as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root (use sudo)"
+check_root() {
+  if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root (use sudo)"
+    exit
+  fi
+}
+
+system_update() {
+  log "Starting system update and package installation."
+  sudo apt-get update >/dev/null 2>&1
+  sudo apt-get install -y ca-certificates curl gnupg lsb-release >/dev/null 2>&1
+  log "System update and package installation completed."
+}
+
+create_user() {
+  log "Creating new user $USER."
+  sudo adduser $USER
+  read -p "Do you want to add $USER to the sudo group? (y/n): " ADD_SUDO
+  if [ "$ADD_SUDO" == "y" ]; then
+    sudo usermod -aG sudo $USER
+    log "Added $USER to the sudo group."
+  fi
+  log "New user $USER created."4
+
+  su - $USER
   exit
-fi
+  log "Switched to user $USER."
+}
 
-TARGET="dev"
-TARGET_DIR="/home/$TARGET"
+generate_ssh_key() {
+  log "Generating SSH key for GitHub access."
+  ssh-keygen -t ed25519 -C "$USER_EMAIL"
+  log "SSH key generated."
 
-PROJECT_NAME="authomatify"
-WEB_DOMAIN="$PROJECT_NAME.com"
-API_DOMAIN="api.$PROJECT_NAME.com"
-NGINX_CONFIG_NAME="$PROJECT_NAME-$TARGET"
+  log "Displaying SSH public key. Please copy it and add it to your GitHub account."
+  cat ~/.ssh/id_ed25519.pub
+  read -p "Please add the above SSH key to your GitHub account. Press Enter to continue once it's done."
+  log "Continuing script after SSH key has been added to GitHub."
 
-FRONTEND_APPLICATION_NAME="$PROJECT_NAME-frontend"
-FRONTEND_APPLICATION_SERVICE_NAME="$FRONTEND_APPLICATION_NAME-$TARGET"
-FRONTEND_APPLICATION_HOST="localhost"
-FRONTEND_APPLICATION_PORT=3000
-FRONTEND_APPLICATION_PROXY_PASS="http://$FRONTEND_APPLICATION_HOST:$FRONTEND_APPLICATION_PORT"
+  su root
+  exit
+  log "Switched back to root user."
+}
 
-BACKEND_APPLICATION_NAME="$PROJECT_NAME-backend"
-BACKEND_APPLICATION_SERVICE_NAME="$BACKEND_APPLICATION_NAME-$TARGET"
-BACKEND_APPLICATION_HOST="localhost"
-BACKEND_APPLICATION_PORT=4000
-BACKEND_APPLICATION_PROXY_PASS="http://$BACKEND_APPLICATION_HOST:$BACKEND_APPLICATION_PORT"
+get_available_application_port() {
+  local APP_NAME=$1
+  local STARTING_PORT=$2
 
-# ------------------------------------------------------------------------------
+  local APPS_COUNT=$(pm2 list | grep -c "$APP_NAME")
+  local ASSIGNED_PORT=$((STARTING_PORT + APPS_COUNT + 1))
 
-log_message "Starting system update and package installation..."
+  echo "$ASSIGNED_PORT"
+}
 
-sudo apt-get update >/dev/null 2>&1
-sudo apt-get install -y ca-certificates curl gnupg lsb-release >/dev/null 2>&1
+install_node_pm2() {
+  log "Installing Node.js and PM2."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash >/dev/null 2>&1
+  source ~/.bashrc
+  [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
+  nvm install --lts >/dev/null 2>&1
+  nvm use --lts >/dev/null 2>&1
+  sudo apt-get install -y npm >/dev/null 2>&1
+  sudo npm install -g pm2 >/dev/null 2>&1
+  log "Node.js and PM2 installation completed."
+}
 
-log_message "System update and package installation completed."
+deploy_backend_application() {
+  BACKEND_APPLICATION_NAME="$PROJECT_NAME-backend"
+  BACKEND_APPLICATION_SERVICE_NAME="$BACKEND_APPLICATION_NAME-$USER"
 
-# ------------------------------------------------------------------------------
+  BACKEND_APPLICATION_PORT=$(get_available_application_port "$BACKEND_APPLICATION_NAME" "4000")
+  BACKEND_APPLICATION_PROXY_PASS=http://localhost:$BACKEND_APPLICATION_PORT
+  echo "Assigned port for new backend application: $BACKEND_APPLICATION_PORT"
 
-log_message "Installing Git..."
+  log "Deploying the backend application."
+  mkdir -p "$USER_DIR"
+  cd "$USER_DIR"
+  git clone git@github.com:$PROJECT_NAME/$BACKEND_APPLICATION_NAME.git $BACKEND_APPLICATION_NAME
+  cd $BACKEND_APPLICATION_NAME
+  npm install >/dev/null 2>&1
+  pm2 start --name "$BACKEND_APPLICATION_SERVICE_NAME" npm -- run start:dev
+  log "Backend application is running on $BACKEND_APPLICATION_PROXY_PASS"
+}
 
-sudo apt-get install -y git >/dev/null 2>&1
+deploy_frontend_application() {
+  FRONTEND_APPLICATION_NAME="$PROJECT_NAME-frontend"
+  FRONTEND_APPLICATION_SERVICE_NAME="$FRONTEND_APPLICATION_NAME-$USER"
 
-log_message "Git installation completed."
+  FRONTEND_APPLICATION_PORT=$(get_available_application_port "$FRONTEND_APPLICATION_NAME" "3000")
+  FRONTEND_APPLICATION_PROXY_PASS=http://localhost:$FRONTEND_APPLICATION_PORT
+  echo "Assigned port for new frontend application: $FRONTEND_APPLICATION_PORT"
 
-log_message "Generating SSH key for GitHub access..."
+  log "Deploying the frontend application."
+  cd "$USER_DIR"
+  git clone git@github.com:$PROJECT_NAME/$FRONTEND_APPLICATION_NAME.git $FRONTEND_APPLICATION_NAME
+  cd $FRONTEND_APPLICATION_NAME
+  npm install >/dev/null 2>&1
+  pm2 start --name "$FRONTEND_APPLICATION_SERVICE_NAME" npm -- run dev
+  log "Frontend application is running on $FRONTEND_APPLICATION_PROXY_PASS"
+}
 
-read -p "Enter your email address for the SSH key: " USER_EMAIL
+configure_nginx() {
+  WEB_DOMAIN="$PROJECT_NAME.com"
+  API_DOMAIN="api.$PROJECT_NAME.com"
+  NGINX_CONFIG_NAME="$PROJECT_NAME-$USER"
 
-ssh-keygen -t ed25519 -C "$USER_EMAIL"
-
-log_message "SSH key generated."
-
-# Display the public key and copy it to the clipboard
-log_message "Displaying SSH public key. Please copy it and add it to your GitHub account."
-cat ~/.ssh/id_ed25519.pub
-
-# Prompt the user to add the SSH key to their GitHub account
-read -p "Please add the above SSH key to your GitHub account. Press Enter to continue once it's done."
-
-log_message "Continuing script after SSH key has been added to GitHub."
-
-# ------------------------------------------------------------------------------
-
-log_message "Setting up Docker repository..."
-
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc >/dev/null 2>&1
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null 2>&1
-
-log_message "Docker repository setup completed."
-
-log_message "Installing Docker..."
-
-sudo apt-get update >/dev/null 2>&1
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null 2>&1
-
-log_message "Docker installation completed."
-
-# ------------------------------------------------------------------------------
-
-log_message "Installing Nginx..."
-
-sudo apt-get install -y nginx >/dev/null 2>&1
-
-log_message "Nginx installation completed."
-
-log_message "Configuring Nginx..."
-
-sudo tee /etc/nginx/sites-available/$NGINX_CONFIG_NAME >/dev/null <<EOF
+  log "Configuring Nginx."
+  sudo tee /etc/nginx/sites-available/$NGINX_CONFIG_NAME >/dev/null <<EOF
 server {
     server_name $WEB_DOMAIN;
 
@@ -132,71 +153,25 @@ server {
     }
 }
 EOF
+  log "Nginx configuration completed."
 
-log_message "Nginx configuration completed."
+  log "Enabling Nginx configuration and reloading the service."
+  sudo ln -s /etc/nginx/sites-available/$NGINX_CONFIG_NAME /etc/nginx/sites-enabled/
+  sudo nginx -t && sudo systemctl reload nginx
+  log "Nginx configuration enabled and service reloaded."
+}
 
-log_message "Enabling Nginx configuration and reloading the service..."
+main() {
+  log "Starting user environment initialization script"
+  check_root
+  system_update
+  create_user
+  generate_ssh_key
+  configure_nginx
+  install_node_pm2
+  deploy_backend_application
+  deploy_frontend_application
+  log "User environment initialization script completed"
+}
 
-sudo ln -s /etc/nginx/sites-available/$NGINX_CONFIG_NAME /etc/nginx/sites-enabled/
-sudo nginx -t && sudo systemctl reload nginx
-
-log_message "Nginx configuration enabled and service reloaded."
-
-# ------------------------------------------------------------------------------
-
-log_message "Installing Node.js and PM2..."
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash >/dev/null 2>&1
-source ~/.bashrc
-[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
-nvm install --lts >/dev/null 2>&1
-nvm use --lts >/dev/null 2>&1
-sudo apt-get install -y npm >/dev/null 2>&1
-sudo npm install -g pm2 >/dev/null 2>&1
-
-log_message "Node.js and PM2 installation completed."
-
-# ------------------------------------------------------------------------------
-
-log_message "Setting up MySQL using Docker..."
-
-# Prompt the user for the MySQL root password
-read -sp "Enter the MySQL root password: " MYSQL_ROOT_PASSWORD
-echo
-
-sudo docker run -d --name mysql \
-  -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
-  -p 3306:3306 \
-  -v db_data:/var/lib/mysql \
-  mysql:8.0 >/dev/null 2>&1
-
-log_message "MySQL setup using Docker completed."
-
-# ------------------------------------------------------------------------------
-
-log_message "Deploying the backend application..."
-
-mkdir -p "$TARGET_DIR"
-cd "$TARGET_DIR"
-git clone git@github.com:$PROJECT_NAME/$BACKEND_APPLICATION_NAME.git $BACKEND_APPLICATION_NAME
-cd $BACKEND_APPLICATION_NAME
-npm install >/dev/null 2>&1
-pm2 start --name "$BACKEND_APPLICATION_SERVICE_NAME" npm -- run start:dev
-
-log_message "Backend application deployment completed."
-
-log_message "Deploying the frontend application..."
-
-cd "$TARGET_DIR"
-git clone git@github.com:$PROJECT_NAME/$FRONTEND_APPLICATION_NAME.git $FRONTEND_APPLICATION_NAME
-cd $FRONTEND_APPLICATION_NAME
-npm install >/dev/null 2>&1
-pm2 start --name "$FRONTEND_APPLICATION_SERVICE_NAME" npm -- run dev
-
-log_message "Frontend application deployment completed."
-
-log_message "All tasks have been successfully completed. Run 'pm2 list' to view the running applications."
-
-# nano server-init.sh
-# chmod +x server-init.sh
-# sudo ./server-init.sh
+main
