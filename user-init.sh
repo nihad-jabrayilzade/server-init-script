e679c88b3677949c5084e06a5a676c250f1c3020
Nihad Jabrayilzade <nihad@Nihads-MacBook-Pro.local>
2024-09-11 00:53:46 +0200

    user init script updated

--- user-init.sh
+++ user-init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+read -p "Enter the username of the new user: " USER
+read -p "Enter the email address for the new user's SSH key: " USER_EMAIL
+
+USER_DIR="/home/$USER"
+PROJECT_NAME="authomatify"
+
 log() {
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
 }
@@ -11,78 +17,143 @@ check_root() {
   fi
 }
 
-update_system_and_install_packages() {
-  log "Starting system update and package installation"
+system_update() {
+  log "Starting system update and package installation."
   sudo apt-get update >/dev/null 2>&1
   sudo apt-get install -y ca-certificates curl gnupg lsb-release >/dev/null 2>&1
   log "System update and package installation completed."
 }
 
-install_git() {
-  log "Installing Git"
-  sudo apt-get install -y git >/dev/null 2>&1
-  log "Git installation completed."
+create_user() {
+  log "Creating new user $USER."
+  sudo adduser $USER
+  read -p "Do you want to add $USER to the sudo group? (y/n): " ADD_SUDO
+  if [ "$ADD_SUDO" == "y" ]; then
+    sudo usermod -aG sudo $USER
+    log "Added $USER to the sudo group."
+  fi
+  log "New user $USER created."
 }
 
-setup_docker_repository() {
-  log "Setting up Docker repository"
-  sudo install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc >/dev/null 2>&1
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null 2>&1
-  log "Docker repository setup completed."
+generate_ssh_key() {
+  log "Generating SSH key for GitHub access for user $USER."
+  sudo su -c "ssh-keygen -t ed25519 -C '$USER_EMAIL'" $USER
+  log "SSH key generated for user $USER."
+
+  log "Displaying SSH public key. Please copy it and add it to your GitHub account."
+  sudo su -c "cat ~/.ssh/id_ed25519.pub" $USER
+  read -p "Please add the above SSH key to your GitHub account. Press Enter to continue once it's done."
+  log "Continuing script after SSH key has been added to GitHub."
 }
 
-install_docker() {
-  log "Installing Docker"
-  sudo apt-get update >/dev/null 2>&1
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null 2>&1
-  log "Docker installation completed."
+get_available_application_port() {
+  local APP_NAME=$1
+  local STARTING_PORT=$2
+
+  local APPS_COUNT=$(pm2 list | grep -c "$APP_NAME")
+  local ASSIGNED_PORT=$((STARTING_PORT + APPS_COUNT + 1))
+
+  echo "$ASSIGNED_PORT"
 }
 
-install_nginx() {
-  log "Installing Nginx"
-  sudo apt-get install -y nginx >/dev/null 2>&1
-  log "Nginx installation completed."
-}
-
-install_node_and_pm2() {
-  log "Installing Node.js and PM2"
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash >/dev/null 2>&1
-  source ~/.bashrc
-  [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
-  nvm install --lts >/dev/null 2>&1
-  nvm use --lts >/dev/null 2>&1
+install_node_pm2() {
+  log "Installing Node.js and PM2 for user $USER."
+  sudo su -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash" $USER
+  sudo su -c "source ~/.bashrc && nvm install --lts && nvm use --lts" $USER
   sudo apt-get install -y npm >/dev/null 2>&1
-  sudo npm install -g pm2 >/dev/null 2>&1
-  log "Node.js and PM2 installation completed."
+  sudo su -c "npm install -g pm2" $USER
+  log "Node.js and PM2 installation completed for user $USER."
 }
 
-setup_database() {
-  log "Setting up MySQL using Docker"
-  read -sp "Enter the MySQL root password: " MYSQL_ROOT_PASSWORD
-  sudo docker run -d --name mysql \
-    -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
-    -p 3306:3306 \
-    -v db_data:/var/lib/mysql \
-    mysql:8.0 >/dev/null 2>&1
-  log "MySQL setup using Docker completed."
+deploy_backend_application() {
+  BACKEND_APPLICATION_NAME="$PROJECT_NAME-backend"
+  BACKEND_APPLICATION_SERVICE_NAME="$BACKEND_APPLICATION_NAME-$USER"
+
+  BACKEND_APPLICATION_PORT=$(get_available_application_port "$BACKEND_APPLICATION_NAME" "4000")
+  BACKEND_APPLICATION_PROXY_PASS=http://localhost:$BACKEND_APPLICATION_PORT
+  echo "Assigned port for new backend application: $BACKEND_APPLICATION_PORT"
+
+  log "Deploying the backend application for user $USER."
+  sudo su -c "mkdir -p $USER_DIR && cd $USER_DIR && git clone git@github.com:$PROJECT_NAME/$BACKEND_APPLICATION_NAME.git $BACKEND_APPLICATION_NAME" $USER
+  sudo su -c "cd $USER_DIR/$BACKEND_APPLICATION_NAME && npm install && pm2 start --name '$BACKEND_APPLICATION_SERVICE_NAME' npm -- run start:dev" $USER
+  log "Backend application is running on $BACKEND_APPLICATION_PROXY_PASS"
+}
+
+deploy_frontend_application() {
+  FRONTEND_APPLICATION_NAME="$PROJECT_NAME-frontend"
+  FRONTEND_APPLICATION_SERVICE_NAME="$FRONTEND_APPLICATION_NAME-$USER"
+
+  FRONTEND_APPLICATION_PORT=$(get_available_application_port "$FRONTEND_APPLICATION_NAME" "3000")
+  FRONTEND_APPLICATION_PROXY_PASS=http://localhost:$FRONTEND_APPLICATION_PORT
+  echo "Assigned port for new frontend application: $FRONTEND_APPLICATION_PORT"
+
+  log "Deploying the frontend application for user $USER."
+  sudo su -c "cd $USER_DIR && git clone git@github.com:$PROJECT_NAME/$FRONTEND_APPLICATION_NAME.git $FRONTEND_APPLICATION_NAME" $USER
+  sudo su -c "cd $USER_DIR/$FRONTEND_APPLICATION_NAME && npm install && pm2 start --name '$FRONTEND_APPLICATION_SERVICE_NAME' npm -- run dev" $USER
+  log "Frontend application is running on $FRONTEND_APPLICATION_PROXY_PASS"
+}
+
+configure_nginx() {
+  WEB_DOMAIN="$PROJECT_NAME.com"
+  API_DOMAIN="api.$PROJECT_NAME.com"
+  NGINX_CONFIG_NAME="$PROJECT_NAME-$USER"
+
+  log "Configuring Nginx."
+  sudo tee /etc/nginx/sites-available/$NGINX_CONFIG_NAME >/dev/null <<EOF
+server {
+    server_name $WEB_DOMAIN;
+
+    location / {
+        proxy_pass $FRONTEND_APPLICATION_PROXY_PASS;
+        proxy_read_timeout 60;
+        proxy_connect_timeout 60;
+        proxy_redirect off;
+
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+    
+    location /_next/static {
+        add_header Cache-Control "public, max-age=3600, immutable";
+        proxy_pass $FRONTEND_APPLICATION_PROXY_PASS/_next/static;
+    }
+}
+
+server {
+    server_name $API_DOMAIN;
+
+    location / {
+        proxy_pass $BACKEND_APPLICATION_PROXY_PASS;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOF
+  log "Nginx configuration completed."
+
+  log "Enabling Nginx configuration and reloading the service."
+  sudo ln -s /etc/nginx/sites-available/$NGINX_CONFIG_NAME /etc/nginx/sites-enabled/
+  sudo nginx -t && sudo systemctl reload nginx
+  log "Nginx configuration enabled and service reloaded."
 }
 
 main() {
-  log "Starting server initialization script"
+  log "Starting user environment initialization script"
   check_root
-  update_system_and_install_packages
-  install_git
-  setup_docker_repository
-  install_docker
-  install_nginx
-  install_node_and_pm2
-  setup_database
-  log "Server initialization script completed"
+  system_update
+  create_user
+  generate_ssh_key
+  configure_nginx
+  install_node_pm2
+  deploy_backend_application
+  deploy_frontend_application
+  log "User environment initialization script completed"
 }
 
 main
