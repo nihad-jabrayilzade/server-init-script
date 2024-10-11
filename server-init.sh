98077c693eeb363e3fd81a5830711f1c640330ca
Nihad Jabrayilzade <cnixad@gmail.com>
2024-10-11 23:10:13 +0200

    renewed

--- server-init.sh
+++ server-init.sh
@@ -1,177 +1,84 @@
 #!/bin/bash
 
-read -p "Enter the username of the new user: " USER
-read -p "Enter the email address for the new user's SSH key: " USER_EMAIL
-
-USER_DIR="/home/$USER"
-PROJECT_NAME="authomatify"
-
 log() {
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
 }
 
 check_root() {
+  # Check if the script is running as root
   if [ "$EUID" -ne 0 ]; then
     echo "Please run this script as root (use sudo)"
     exit
   fi
 }
 
-system_update() {
-  log "Starting system update and package installation."
-  sudo apt-get update >/dev/null 2>&1
-  sudo apt-get install -y ca-certificates curl gnupg lsb-release >/dev/null 2>&1
-  log "System update and package installation completed."
+install_system_packages() {
+  log "Installing system packages"
+  apt-get update >/dev/null 2>&1
+  apt-get install -y ca-certificates curl gnupg lsb-release >/dev/null 2>&1
+  log "System packages installation completed"
 }
 
-create_user() {
-  log "Creating new user $USER."
-  sudo adduser $USER
-  read -p "Do you want to add $USER to the sudo group? (y/n): " ADD_SUDO
-  if [ "$ADD_SUDO" == "y" ]; then
-    sudo usermod -aG sudo $USER
-    log "Added $USER to the sudo group."
-  fi
-  log "New user $USER created."4
-
-  su - $USER
-  exit
-  log "Switched to user $USER."
+install_git() {
+  log "Installing Git"
+  apt-get install -y git >/dev/null 2>&1
+  log "Git installation completed"
 }
 
-generate_ssh_key() {
-  log "Generating SSH key for GitHub access."
-  ssh-keygen -t ed25519 -C "$USER_EMAIL"
-  log "SSH key generated."
-
-  log "Displaying SSH public key. Please copy it and add it to your GitHub account."
-  cat ~/.ssh/id_ed25519.pub
-  read -p "Please add the above SSH key to your GitHub account. Press Enter to continue once it's done."
-  log "Continuing script after SSH key has been added to GitHub."
-
-  su root
-  exit
-  log "Switched back to root user."
+configure_docker() {
+  log "Starting Docker configuration"
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | tee /etc/apt/keyrings/docker.asc >/dev/null 2>&1
+  chmod a+r /etc/apt/keyrings/docker.asc
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    tee /etc/apt/sources.list.d/docker.list >/dev/null 2>&1
+  apt-get update >/dev/null 2>&1
+  apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null 2>&1
+  log "Docker configuration completed"
 }
 
-get_available_application_port() {
-  local APP_NAME=$1
-  local STARTING_PORT=$2
-
-  local APPS_COUNT=$(pm2 list | grep -c "$APP_NAME")
-  local ASSIGNED_PORT=$((STARTING_PORT + APPS_COUNT + 1))
-
-  echo "$ASSIGNED_PORT"
+install_nginx() {
+  log "Installing Nginx"
+  apt-get install -y nginx >/dev/null 2>&1
+  log "Nginx installation completed"
 }
 
-install_node_pm2() {
-  log "Installing Node.js and PM2."
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash >/dev/null 2>&1
-  source ~/.bashrc
-  [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
+configure_node_environment() {
+  log "Configuring Node environment"
+  curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash >/dev/null 2>&1
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   nvm install --lts >/dev/null 2>&1
   nvm use --lts >/dev/null 2>&1
-  sudo apt-get install -y npm >/dev/null 2>&1
-  sudo npm install -g pm2 >/dev/null 2>&1
-  log "Node.js and PM2 installation completed."
+  apt-get install -y npm >/dev/null 2>&1
+  npm install -g pm2 >/dev/null 2>&1
+  log "Node configuration completed"
 }
 
-deploy_backend_application() {
-  BACKEND_APPLICATION_NAME="$PROJECT_NAME-backend"
-  BACKEND_APPLICATION_SERVICE_NAME="$BACKEND_APPLICATION_NAME-$USER"
-
-  BACKEND_APPLICATION_PORT=$(get_available_application_port "$BACKEND_APPLICATION_NAME" "4000")
-  BACKEND_APPLICATION_PROXY_PASS=http://localhost:$BACKEND_APPLICATION_PORT
-  echo "Assigned port for new backend application: $BACKEND_APPLICATION_PORT"
-
-  log "Deploying the backend application."
-  mkdir -p "$USER_DIR"
-  cd "$USER_DIR"
-  git clone git@github.com:$PROJECT_NAME/$BACKEND_APPLICATION_NAME.git $BACKEND_APPLICATION_NAME
-  cd $BACKEND_APPLICATION_NAME
-  npm install >/dev/null 2>&1
-  pm2 start --name "$BACKEND_APPLICATION_SERVICE_NAME" npm -- run start:dev
-  log "Backend application is running on $BACKEND_APPLICATION_PROXY_PASS"
-}
-
-deploy_frontend_application() {
-  FRONTEND_APPLICATION_NAME="$PROJECT_NAME-frontend"
-  FRONTEND_APPLICATION_SERVICE_NAME="$FRONTEND_APPLICATION_NAME-$USER"
-
-  FRONTEND_APPLICATION_PORT=$(get_available_application_port "$FRONTEND_APPLICATION_NAME" "3000")
-  FRONTEND_APPLICATION_PROXY_PASS=http://localhost:$FRONTEND_APPLICATION_PORT
-  echo "Assigned port for new frontend application: $FRONTEND_APPLICATION_PORT"
-
-  log "Deploying the frontend application."
-  cd "$USER_DIR"
-  git clone git@github.com:$PROJECT_NAME/$FRONTEND_APPLICATION_NAME.git $FRONTEND_APPLICATION_NAME
-  cd $FRONTEND_APPLICATION_NAME
-  npm install >/dev/null 2>&1
-  pm2 start --name "$FRONTEND_APPLICATION_SERVICE_NAME" npm -- run dev
-  log "Frontend application is running on $FRONTEND_APPLICATION_PROXY_PASS"
-}
-
-configure_nginx() {
-  WEB_DOMAIN="$PROJECT_NAME.com"
-  API_DOMAIN="api.$PROJECT_NAME.com"
-  NGINX_CONFIG_NAME="$PROJECT_NAME-$USER"
-
-  log "Configuring Nginx."
-  sudo tee /etc/nginx/sites-available/$NGINX_CONFIG_NAME >/dev/null <<EOF
-server {
-    server_name $WEB_DOMAIN;
-
-    location / {
-        proxy_pass $FRONTEND_APPLICATION_PROXY_PASS;
-        proxy_read_timeout 60;
-        proxy_connect_timeout 60;
-        proxy_redirect off;
-
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-    }
-    
-    location /_next/static {
-        add_header Cache-Control "public, max-age=3600, immutable";
-        proxy_pass $FRONTEND_APPLICATION_PROXY_PASS/_next/static;
-    }
-}
-
-server {
-    server_name $API_DOMAIN;
-
-    location / {
-        proxy_pass $BACKEND_APPLICATION_PROXY_PASS;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-    }
-}
-EOF
-  log "Nginx configuration completed."
-
-  log "Enabling Nginx configuration and reloading the service."
-  sudo ln -s /etc/nginx/sites-available/$NGINX_CONFIG_NAME /etc/nginx/sites-enabled/
-  sudo nginx -t && sudo systemctl reload nginx
-  log "Nginx configuration enabled and service reloaded."
+configure_database() {
+  log "Starting Database configuration using docker"
+  read -sp "Enter the MySQL root password: " MYSQL_ROOT_PASSWORD
+  echo
+  sudo docker run -d --name mysql \
+    -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+    -p 3306:3306 \
+    -v db_data:/var/lib/mysql \
+    mysql:8.0 >/dev/null 2>&1
+  log "Database configuration completed"
 }
 
 main() {
-  log "Starting user environment initialization script"
+  log "Starting server initialization tasks"
   check_root
-  system_update
-  create_user
-  generate_ssh_key
-  configure_nginx
-  install_node_pm2
-  deploy_backend_application
-  deploy_frontend_application
-  log "User environment initialization script completed"
+  install_system_packages
+  install_git
+  install_nginx
+  configure_node_environment
+  configure_docker
+  configure_database
+  log "Server initialization tasks completed"
 }
 
 main
--- user-init.sh
+++ user-init.sh
@@ -6,8 +6,18 @@ read -p "Enter the email address for the new user's SSH key: " USER_EMAIL
 USER_DIR="/home/$USER"
 PROJECT_NAME="authomatify"
 
+NVM_DIR_COMMAND='[ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm"'
+
 log() {
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
+
+run_as_user() {
+  sudo -u "$USER" bash -c "$@"
+}
+
+run_as_user_nvm() {
+  run_as_user "export NVM_DIR=\"\$($NVM_DIR_COMMAND)\" && . \"\$NVM_DIR/nvm.sh\" && $1"
 }
 
 check_root() {
@@ -17,142 +27,142 @@ check_root() {
   fi
 }
 
-system_update() {
-  log "Starting system update and package installation."
-  sudo apt-get update >/dev/null 2>&1
-  sudo apt-get install -y ca-certificates curl gnupg lsb-release >/dev/null 2>&1
-  log "System update and package installation completed."
+install_system_packages() {
+  log "Starting system packages installation"
+  apt-get update >/dev/null 2>&1
+  apt-get install -y ca-certificates curl gnupg lsb-release >/dev/null 2>&1
+  log "System packages installation completed"
 }
 
 create_user() {
-  log "Creating new user $USER."
-  sudo adduser $USER
+  log "Creating new user $USER"
+  adduser $USER
   read -p "Do you want to add $USER to the sudo group? (y/n): " ADD_SUDO
   if [ "$ADD_SUDO" == "y" ]; then
-    sudo usermod -aG sudo $USER
-    log "Added $USER to the sudo group."
+    usermod -aG sudo $USER
+    log "Added $USER to the sudo group"
   fi
-  log "New user $USER created."
+  log "New user $USER created"
+}
+
+
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
 }
 
 generate_ssh_key() {
-  log "Generating SSH key for GitHub access for user $USER."
-  sudo su -c "ssh-keygen -t ed25519 -C '$USER_EMAIL'" $USER
-  log "SSH key generated for user $USER."
-
-  log "Displaying SSH public key. Please copy it and add it to your GitHub account."
-  sudo su -c "cat ~/.ssh/id_ed25519.pub" $USER
-  read -p "Please add the above SSH key to your GitHub account. Press Enter to continue once it's done."
-  log "Continuing script after SSH key has been added to GitHub."
+  log "Generating SSH key for GitHub access for user $USER"
+  run_as_user "ssh-keygen -t ed25519 -C $USER_EMAIL"
+  log "Displaying SSH public key. Please copy it and add it to your GitHub account"
+  run_as_user "cat ~/.ssh/id_ed25519.pub"
+  read -p "Please add the above SSH key to your GitHub account. Press Enter to continue once it's done"
+  log "Continuing script after SSH key has been added to GitHub"
 }
 
 get_available_application_port() {
   local APP_NAME=$1
   local STARTING_PORT=$2
 
-  local APPS_COUNT=$(pm2 list | grep -c "$APP_NAME")
+  local APPS_COUNT=$(run_as_user_nvm "pm2 list | grep -c '$APP_NAME'")
   local ASSIGNED_PORT=$((STARTING_PORT + APPS_COUNT + 1))
 
   echo "$ASSIGNED_PORT"
 }
 
-install_node_pm2() {
-  log "Installing Node.js and PM2 for user $USER."
-  sudo su -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash" $USER
-  sudo su -c "source ~/.bashrc && nvm install --lts && nvm use --lts" $USER
-  sudo apt-get install -y npm >/dev/null 2>&1
-  sudo su -c "npm install -g pm2" $USER
-  log "Node.js and PM2 installation completed for user $USER."
+configure_node_environment() {
+  NVM_VERSION="v0.39.5"
+  NODE_VERSION="lts/*"
+  NVM_INSTALL_URL="https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh"
+
+  log "Starting Node ennvironment configuration for user $USER"
+  run_as_user "export NVM_DIR=\"\$($NVM_DIR_COMMAND)\" && curl -o- -s $NVM_INSTALL_URL | bash &> /dev/null"
+  run_as_user_nvm "nvm install $NODE_VERSION &> /dev/null"
+  run_as_user_nvm "npm install -g pm2 &> /dev/null"
+  log "Node ennvironment configuration completed for user $USER"
 }
 
-deploy_backend_application() {
+deploy_applications() {
   BACKEND_APPLICATION_NAME="$PROJECT_NAME-backend"
   BACKEND_APPLICATION_SERVICE_NAME="$BACKEND_APPLICATION_NAME-$USER"
+  BACKEND_APPLICATION_OFFSET_PORT=4000
+  BACKEND_APPLICATION_PORT=$(get_available_application_port $BACKEND_APPLICATION_NAME $BACKEND_APPLICATION_OFFSET_PORT)
+  BACKEND_APPLICATION_PROXY_PASS="http://localhost:$BACKEND_APPLICATION_PORT"
 
-  BACKEND_APPLICATION_PORT=$(get_available_application_port "$BACKEND_APPLICATION_NAME" "4000")
-  BACKEND_APPLICATION_PROXY_PASS=http://localhost:$BACKEND_APPLICATION_PORT
-  echo "Assigned port for new backend application: $BACKEND_APPLICATION_PORT"
-
-  log "Deploying the backend application for user $USER."
-  sudo su -c "mkdir -p $USER_DIR && cd $USER_DIR && git clone git@github.com:$PROJECT_NAME/$BACKEND_APPLICATION_NAME.git $BACKEND_APPLICATION_NAME" $USER
-  sudo su -c "cd $USER_DIR/$BACKEND_APPLICATION_NAME && npm install && pm2 start --name '$BACKEND_APPLICATION_SERVICE_NAME' npm -- run start:dev" $USER
+  run_as_user_nvm "mkdir -p $USER_DIR && cd $USER_DIR && git clone git@github.com:$PROJECT_NAME/$BACKEND_APPLICATION_NAME.git $BACKEND_APPLICATION_NAME &> /dev/null"
+  log "Deploying the backend application for user $USER"
+  run_as_user_nvm "cd $USER_DIR/$BACKEND_APPLICATION_NAME && npm install && pm2 start --name '$BACKEND_APPLICATION_SERVICE_NAME' npm -- run start:dev"
   log "Backend application is running on $BACKEND_APPLICATION_PROXY_PASS"
-}
 
-deploy_frontend_application() {
+  log "Deploying the frontend application for user $USER"
   FRONTEND_APPLICATION_NAME="$PROJECT_NAME-frontend"
   FRONTEND_APPLICATION_SERVICE_NAME="$FRONTEND_APPLICATION_NAME-$USER"
-
   FRONTEND_APPLICATION_PORT=$(get_available_application_port "$FRONTEND_APPLICATION_NAME" "3000")
-  FRONTEND_APPLICATION_PROXY_PASS=http://localhost:$FRONTEND_APPLICATION_PORT
-  echo "Assigned port for new frontend application: $FRONTEND_APPLICATION_PORT"
-
-  log "Deploying the frontend application for user $USER."
-  sudo su -c "cd $USER_DIR && git clone git@github.com:$PROJECT_NAME/$FRONTEND_APPLICATION_NAME.git $FRONTEND_APPLICATION_NAME" $USER
-  sudo su -c "cd $USER_DIR/$FRONTEND_APPLICATION_NAME && npm install && pm2 start --name '$FRONTEND_APPLICATION_SERVICE_NAME' npm -- run dev" $USER
+  FRONTEND_APPLICATION_PROXY_PASS="http://localhost:$FRONTEND_APPLICATION_PORT"
+  run_as_user_nvm "cd $USER_DIR && git clone git@github.com:$PROJECT_NAME/$FRONTEND_APPLICATION_NAME.git $FRONTEND_APPLICATION_NAME &> /dev/null"
+  run_as_user_nvm "cd $USER_DIR/$FRONTEND_APPLICATION_NAME && npm install && pm2 start --name '$FRONTEND_APPLICATION_SERVICE_NAME' npm -- run dev"
   log "Frontend application is running on $FRONTEND_APPLICATION_PROXY_PASS"
 }
 
 configure_nginx() {
-  WEB_DOMAIN="$PROJECT_NAME.com"
-  API_DOMAIN="api.$PROJECT_NAME.com"
+  WEB_DOMAIN="$USER.$PROJECT_NAME.com"
+  API_DOMAIN="$USER.api.$PROJECT_NAME.com"
   NGINX_CONFIG_NAME="$PROJECT_NAME-$USER"
 
-  log "Configuring Nginx."
-  sudo tee /etc/nginx/sites-available/$NGINX_CONFIG_NAME >/dev/null <<EOF
-server {
-    server_name $WEB_DOMAIN;
+  log "Configuring Nginx"
+  tee /etc/nginx/sites-available/$NGINX_CONFIG_NAME >/dev/null <<EOF
+    server {
+        server_name $WEB_DOMAIN;
 
-    location / {
-        proxy_pass $FRONTEND_APPLICATION_PROXY_PASS;
-        proxy_read_timeout 60;
-        proxy_connect_timeout 60;
-        proxy_redirect off;
+        location / {
+            proxy_pass $FRONTEND_APPLICATION_PROXY_PASS;
+            proxy_read_timeout 60;
+            proxy_connect_timeout 60;
+            proxy_redirect off;
 
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host \$host;
+            proxy_cache_bypass \$http_upgrade;
+        }
+        
+        location /_next/static {
+            add_header Cache-Control "public, max-age=3600, immutable";
+            proxy_pass $FRONTEND_APPLICATION_PROXY_PASS/_next/static;
+        }
     }
-    
-    location /_next/static {
-        add_header Cache-Control "public, max-age=3600, immutable";
-        proxy_pass $FRONTEND_APPLICATION_PROXY_PASS/_next/static;
-    }
-}
 
-server {
-    server_name $API_DOMAIN;
+    server {
+        server_name $API_DOMAIN;
 
-    location / {
-        proxy_pass $BACKEND_APPLICATION_PROXY_PASS;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
+        location / {
+            proxy_pass $BACKEND_APPLICATION_PROXY_PASS;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host \$host;
+            proxy_cache_bypass \$http_upgrade;
+        }
     }
-}
 EOF
-  log "Nginx configuration completed."
 
-  log "Enabling Nginx configuration and reloading the service."
-  sudo ln -s /etc/nginx/sites-available/$NGINX_CONFIG_NAME /etc/nginx/sites-enabled/
-  sudo nginx -t && sudo systemctl reload nginx
-  log "Nginx configuration enabled and service reloaded."
+  log "Nginx configuration completed"
+
+  log "Enabling Nginx configuration and reloading the service"
+  ln -s /etc/nginx/sites-available/$NGINX_CONFIG_NAME /etc/nginx/sites-enabled/
+  nginx -t && systemctl reload nginx
+  log "Nginx configuration enabled and service reloaded"
 }
 
 main() {
   log "Starting user environment initialization script"
   check_root
-  system_update
+  install_system_packages
   create_user
   generate_ssh_key
+  configure_node_environment
+  deploy_applications
   configure_nginx
-  install_node_pm2
-  deploy_backend_application
-  deploy_frontend_application
   log "User environment initialization script completed"
 }
 
